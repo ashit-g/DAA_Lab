bf96fe36fd91b9ebf3370b29327458c34d803860
ashit-g <ashit.gupta775@gmail.com>
2026-01-29 12:16:58 +0530

    Q4.cpp
    max subarray sum

---
+++ Assignment 1/Q4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n) {
+    int currentMax = arr[0];
+    int maxSoFar = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        currentMax = max(arr[i], currentMax + arr[i]);
+        maxSoFar = max(maxSoFar, currentMax);
+    }
+
+    return maxSoFar;
+}
+
+int main() {
+    int arr[] = {-2, -5, 6, -2, -3, 1, 5, -6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Maximum subarray sum: " << maxSubarraySum(arr, n) << endl;
+
+    return 0;
+}
+
+//OUTPUT
+//Maximum subarray sum: 7
